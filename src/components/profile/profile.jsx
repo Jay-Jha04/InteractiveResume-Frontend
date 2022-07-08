ca5f44a52facbd8845c83288ed098210988a8e6a
Jay Jha <jayjha@jays-MacBook-Pro.local>
2022-07-08 16:53:16 +0530

    adding loader for profile page

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./header";
 import { getProfile, getProfileImage } from "../../services/profile";
+import { Bars } from "react-loading-icons";
 
 const Profile = () => {
   const [profile, setProfile] = useState({});
@@ -22,6 +23,18 @@ const Profile = () => {
 
   return (
     <>
+      {Object.keys(profile).length === 0 && (
+        <div className="row justify-content-center mt-5">
+          <Bars
+            height="10em"
+            fill="#6070ee"
+            stroke="transparent"
+            speed="1"
+            fillOpacity="1"
+            strokeOpacity="1"
+          />
+        </div>
+      )}
       {Object.keys(profile).length > 0 && (
         <>
           <Header data={profile} profileImage={profileImage} />
