565f043285f47a59e6c68861045749b4ab51823b
Jay Jha <jayjha@jays-MacBook-Pro.local>
2022-07-10 19:29:16 +0530

    increasing timeout for url request

--- src/services/http.js
+++ src/services/http.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_INTERACTIVERESUME_BASEURL,
-  timeout: 1000,
+  timeout: 10000,
 });
 
 export const setTokenHeader = (token) => {
